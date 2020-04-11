0cae8405d98162633f956cf3a90697363d1dd5c0
lelani82 <lelaniparker@hotmail.com>
2020-04-11 18:57:47 +1000

    adding online games into relax tab

--- screens/RelaxScreen.js
+++ screens/RelaxScreen.js
@@ -1,28 +1,59 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { Ionicons } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { RectButton, ScrollView } from "react-native-gesture-handler";
 
 export default function RelaxScreen() {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+    >
       <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
+        icon="md-compass"
+        label="Free Online Games"
+        onPress={() =>
+          WebBrowser.openBrowserAsync(
+            "https://www.freeonlinegames.com/embed/149495"
+          )
+        }
       />
 
       <OptionButton
         icon="md-compass"
+        label="Drawize - Free Online Drawing Game"
+        onPress={() => WebBrowser.openBrowserAsync("https://www.drawize.com/")}
+      />
+
+      <OptionButton
+        icon="md-compass"
+        label="DoodleAddicts - Drawing Challenges"
+        onPress={() =>
+          WebBrowser.openBrowserAsync(
+            "https://www.doodleaddicts.com/drawing-challenges/"
+          )
+        }
+      />
+
+      <OptionButton
+        icon="md-compass"
+        label="InkTober - 31 Day Drawing Challenge"
+        onPress={() => WebBrowser.openBrowserAsync("https://inktober.com/")}
+      />
+
+      <OptionButton
+        icon="md-school"
         label="Read the React Navigation documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
+        onPress={() =>
+          WebBrowser.openBrowserAsync("https://reactnavigation.org")
+        }
       />
 
       <OptionButton
         icon="ios-chatboxes"
         label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
+        onPress={() => WebBrowser.openBrowserAsync("https://forums.expo.io")}
         isLastOption
       />
     </ScrollView>
@@ -31,8 +62,11 @@ export default function RelaxScreen() {
 
 function OptionButton({ icon, label, onPress, isLastOption }) {
   return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
+    <RectButton
+      style={[styles.option, isLastOption && styles.lastOption]}
+      onPress={onPress}
+    >
+      <View style={{ flexDirection: "row" }}>
         <View style={styles.optionIconContainer}>
           <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
         </View>
@@ -47,7 +81,7 @@ function OptionButton({ icon, label, onPress, isLastOption }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa",
   },
   contentContainer: {
     paddingTop: 15,
@@ -56,19 +90,19 @@ const styles = StyleSheet.create({
     marginRight: 12,
   },
   option: {
-    backgroundColor: '#fdfdfd',
+    backgroundColor: "#fdfdfd",
     paddingHorizontal: 15,
     paddingVertical: 15,
     borderWidth: StyleSheet.hairlineWidth,
     borderBottomWidth: 0,
-    borderColor: '#ededed',
+    borderColor: "#ededed",
   },
   lastOption: {
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
   optionText: {
     fontSize: 15,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginTop: 1,
   },
 });
